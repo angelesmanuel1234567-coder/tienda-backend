08d0539928a44445d1f6742297adcdef7186d7e4
angelesmanuel1234567-coder <angelesmanuel1234567@gmail.com>
2026-05-07 08:35:11 -0500

    Update MySQL connection configuration

--- db.js
+++ db.js
@@ -1,6 +1,12 @@
 const mysql = require("mysql2");
 
-const connection = mysql.createConnection(process.env.MYSQL_URL);
+const connection = mysql.createConnection({
+  host: "turntable.proxy.rlwy.net",
+  user: "root",
+  password: "IuzwsbcAtiTttevSKlnkmizyBkbYGrdc",
+  database: "railway",
+  port: 47399
+});
 
 connection.connect(err => {
   if (err) {
